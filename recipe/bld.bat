9e714dd3d725ba491a48f5478878e479082659a8
Daniel Ching <carterbox@users.noreply.github.com>
2023-12-03 17:00:58 -0600

    REF: Correctly distribute Windows artifacts into outputs
    Redistribute Windows artifacts according to whether they
    are needed at compile-link-time OR runtime. For Windows,
    DLLs are only needed at Windows runtime. LIBs and headers
    are only needed at build time. For Unix, SOs are needed both
    at build and runtime.
    
    Include target_name directory for Windows to support
    cross-compile (in future?).

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,10 @@
 if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_LIB%\x64 mkdir %LIBRARY_LIB%\x64
+if errorlevel 1 exit /b 1
 
-move lib\x64\* %LIBRARY_LIB%
+copy lib\x64\* %LIBRARY_LIB%
+copy lib\x64\* %LIBRARY_LIB%\x64
 move bin\* %LIBRARY_BIN%
 move include\cooperative_groups %LIBRARY_INC%
 move include\* %LIBRARY_INC%
+if errorlevel 1 exit /b 1
