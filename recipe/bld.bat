611c911fa112b7969a6e8020cb5d7700078e043e
Daniel Ching <carterbox@users.noreply.github.com>
2023-12-08 22:45:24 -0600

    REF: No target_name subdirectory on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,7 @@
 if not exist %PREFIX% mkdir %PREFIX%
-if not exist %LIBRARY_LIB%\x64 mkdir %LIBRARY_LIB%\x64
 if errorlevel 1 exit /b 1
 
-copy lib\x64\* %LIBRARY_LIB%
-copy lib\x64\* %LIBRARY_LIB%\x64
+move lib\x64\* %LIBRARY_LIB%
 move bin\* %LIBRARY_BIN%
 move include\cooperative_groups %LIBRARY_INC%
 move include\* %LIBRARY_INC%
