c77d4efeafc6bca6f0a5c268dcae740b2dbd4dfa
Daniel Ching <carterbox@users.noreply.github.com>
2023-12-13 12:28:59 -0600

    Update bld.bat to use environment variable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 if not exist %PREFIX% mkdir %PREFIX%
-if errorlevel 1 exit /b 1
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 
 move lib\x64\* %LIBRARY_LIB%
 move bin\* %LIBRARY_BIN%
 move include\cooperative_groups %LIBRARY_INC%
 move include\* %LIBRARY_INC%
-if errorlevel 1 exit /b 1
+IF %ERRORLEVEL% NEQ 0 exit /b 1
