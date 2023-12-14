ba06b61454be0940f2e73e023c56b3ca8241eabe
jakirkham <jakirkham@gmail.com>
2023-12-13 18:17:35 -0800

    Lowercase `bld.bat` error check

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,4 @@ move lib\x64\* %LIBRARY_LIB%
 move bin\* %LIBRARY_BIN%
 move include\cooperative_groups %LIBRARY_INC%
 move include\* %LIBRARY_INC%
-IF %ERRORLEVEL% NEQ 0 exit /b 1
+if %errorlevel% neq 0 exit /b 1
